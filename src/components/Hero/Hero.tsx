c2bd553473a950ef23e67d3c7766e17a9520e579
PriyaKatariya <priyakatariya2007@gmail.com>
2026-06-22 21:43:53 +0530

    fix: remove unused FaCode import to resolve build error

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
-import { FaGithub, FaLinkedin, FaEnvelope, FaCode } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 
 const HeroContainer = styled.section`
   min-height: calc(100vh - 65px);
